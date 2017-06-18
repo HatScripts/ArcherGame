d5ffe3a451e981c79214cd301fde6cb4c29ac148
HatScripts <contact@hatscripts.com>
2017-06-18 20:26:15 +1000

    Remove `main` method, add "debug" command line arg which makes debug mode on by default, condense the adding of key and mouse event filters

--- src/main/java/com/hatscripts/archergame/window/Main.java
+++ src/main/java/com/hatscripts/archergame/window/Main.java
@@ -3,35 +3,32 @@ package com.hatscripts.archergame.window;
 import com.hatscripts.archergame.input.KeyInput;
 import com.hatscripts.archergame.input.MouseInput;
 import javafx.application.Application;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	private Game game;
 
-	public static void main(String[] args) {
-		launch(args);
-	}
-
 	@Override
 	public void start(Stage stage) throws Exception {
 		Group root = new Group();
 		Scene scene = new Scene(root);
-		KeyInput keyInput = new KeyInput();
-		scene.setOnKeyPressed(keyInput);
-		scene.setOnKeyReleased(keyInput);
-		scene.setOnKeyTyped(keyInput);
 
-		MouseInput mouseInput = new MouseInput();
-		scene.setOnMouseMoved(mouseInput);
-		scene.setOnMouseClicked(mouseInput);
-		scene.setOnMouseEntered(mouseInput);
-		scene.setOnMouseExited(mouseInput);
-		scene.setOnMousePressed(mouseInput);
-		scene.setOnMouseReleased(mouseInput);
+		SimpleBooleanProperty debug = new SimpleBooleanProperty(
+				getParameters().getUnnamed().contains("debug"));
 
-		game = new Game(640, 352, keyInput, mouseInput);
+		KeyInput keyInput = new KeyInput(debug);
+		scene.addEventFilter(KeyEvent.ANY, keyInput);
+
+		MouseInput mouseInput = new MouseInput(stage.xProperty(), stage.yProperty());
+		scene.addEventFilter(MouseEvent.ANY, mouseInput);
+
+		double height = 480;
+		game = new Game(height * (16d / 9), height, keyInput, mouseInput, debug);
 		root.getChildren().add(game);
 
 		stage.setTitle("Archer Game");
