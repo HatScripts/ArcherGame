8a098a09e4a21d19655786eaffcfa38f10f5f1b5
HatScripts <contact@hatscripts.com>
2017-06-18 20:12:03 +1000

    Move color constants to static inner class, give the player a bow, fix incorrect com.sun import

--- src/main/java/com/hatscripts/archergame/objects/list/Player.java
+++ src/main/java/com/hatscripts/archergame/objects/list/Player.java
@@ -5,22 +5,28 @@ import com.hatscripts.archergame.input.MouseInput;
 import com.hatscripts.archergame.input.MovementKey;
 import com.hatscripts.archergame.objects.GameObject;
 import com.hatscripts.archergame.objects.ObjectType;
-import com.sun.javafx.geom.Dimension2D;
+import javafx.geometry.Dimension2D;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 
 public class Player extends GameObject {
-	// TODO: Give the player a bow so that the title "Archer Game" is accurate
-	private static final Color COLOR_SKIN = Color.color(0.94, 0.76, 0.68);
-	private static final Color COLOR_HAIR = Color.color(0.21, 0.18, 0.14);
-	private static final Color COLOR_PUPIL = Color.color(0.29, 0.29, 0.29);
-	private static final Color COLOR_SCLERA = Color.WHITE;
-	private static final Color COLOR_MOUTH = COLOR_SKIN.interpolate(Color.BLACK, 0.5);
-	private static final Color COLOR_TOP = Color.color(0.16, 0.16, 0.16);
+	private static final class Colors {
+		private static final Color SKIN = Color.color(0.94, 0.76, 0.68);
+		private static final Color MOUTH = SKIN.interpolate(Color.BLACK, 0.5);
+		private static final Color HAIR = Color.color(0.21, 0.18, 0.14);
+		private static final Color PUPIL = Color.color(0.29, 0.29, 0.29);
+		private static final Color SCLERA = Color.WHITE;
+		private static final Color TOP = Color.color(0.16, 0.16, 0.16);
+		private static final Color BOW = Color.color(0.5, 0.4, 0.3);
+		private static final Color BOWSTRING = Color.color(1, 1, 1, 0.75);
+	}
+
 	private static final Dimension2D EYE = new Dimension2D(6, 4);
-	private static final Dimension2D PUPIL = new Dimension2D(EYE.width / 1.5f, EYE.height);
+	private static final Dimension2D PUPIL = new Dimension2D(EYE.getWidth() / 1.5, EYE.getHeight());
 	private static final Dimension2D MOUTH = new Dimension2D(5, 1);
+	private static final double BOW_SIZE = ObjectType.PLAYER.getHeight();
 	private final KeyInput keyInput;
 	private final MouseInput mouseInput;
 
@@ -34,26 +40,43 @@ public class Player extends GameObject {
 		// TODO: Add eyelids; blinking, squinting, etc.
 
 		/* Sclera */
-		g.setFill(COLOR_SCLERA);
-		g.fillRect(x, y, EYE.width, EYE.height);
+		g.setFill(Colors.SCLERA);
+		g.fillRect(x, y, EYE.getWidth(), EYE.getHeight());
 
 		/* Pupil */
 		// TODO: Make pupil follow cursor vertically as well, not just horizontally.
-		g.setFill(COLOR_PUPIL);
-		double pupilX = mouseInput.mouseLocation().getX();
+		g.setFill(Colors.PUPIL);
+		double pupilX = mouseInput.location().getX();
 		pupilX = Math.max(pupilX, x);
-		pupilX = Math.min(pupilX, x + EYE.width - PUPIL.width);
-		g.fillRect(pupilX, y, PUPIL.width, PUPIL.height);
+		pupilX = Math.min(pupilX, x + EYE.getWidth() - PUPIL.getWidth());
+		g.fillRect(pupilX, y, PUPIL.getWidth(), PUPIL.getHeight());
 
 		/* Eyebrow */
-		g.setFill(COLOR_HAIR);
-		g.fillRect(x, y - 2, EYE.width, 2);
+		g.setFill(Colors.HAIR);
+		g.fillRect(x, y - 2, EYE.getWidth(), 2);
 	}
 
 	private static void drawMouth(GraphicsContext g, double x, double y) {
-		g.setFill(COLOR_MOUTH);
-		g.fillRect(x, y, MOUTH.width, MOUTH.height);
-		g.fillRect(x + 1, y, MOUTH.width - 2, MOUTH.height);
+		g.setFill(Colors.MOUTH);
+		g.fillRect(x, y, MOUTH.getWidth(), MOUTH.getHeight());
+		g.fillRect(x + 1, y, MOUTH.getWidth() - 2, MOUTH.getHeight());
+	}
+
+	private void drawBow(GraphicsContext g) {
+		g.save();
+		Point2D center = getCenterPoint();
+		double angleToMouse = this.angleTo(mouseInput.location());
+		double x = center.getX() - BOW_SIZE / 2;
+		double y = center.getY() - BOW_SIZE / 2;
+		g.setStroke(Colors.BOWSTRING);
+		g.setLineWidth(1);
+		int arcExtent = 100;
+		double startAngle = angleToMouse - arcExtent / 2 + 90;
+		g.strokeArc(x, y, BOW_SIZE, BOW_SIZE, startAngle, arcExtent, ArcType.CHORD);
+		g.setStroke(Colors.BOW);
+		g.setLineWidth(3);
+		g.strokeArc(x, y, BOW_SIZE, BOW_SIZE, startAngle, arcExtent, ArcType.OPEN);
+		g.restore();
 	}
 
 	@Override
@@ -69,7 +92,7 @@ public class Player extends GameObject {
 
 	@Override
 	public void render(GraphicsContext g) {
-		g.setFill(COLOR_SKIN);
+		g.setFill(Colors.SKIN);
 		g.fillRect(x, y, width, width);
 
 		/* Eyes */
@@ -80,24 +103,32 @@ public class Player extends GameObject {
 		drawEye(g, rightEyeX, eyeY, mouseInput);
 
 		/* Mouth */
-		drawMouth(g, x + (width / 2) - (MOUTH.width / 2), y + (width / 4) * 3 + 1);
+		drawMouth(g, x + (width / 2) - (MOUTH.getWidth() / 2), y + (width / 4) * 3 + 1);
 
 		/* Hair */
-		g.setFill(COLOR_HAIR);
+		g.setFill(Colors.HAIR);
 		double[] xPoints = {x, x, x + 24, x + width, x + width};
 		double[] yPoints = {y, y + 20, y + 10, y + 18, y};
 		g.fillPolygon(xPoints, yPoints, xPoints.length);
 
 		/* Top */
-		g.setFill(COLOR_TOP);
+		g.setFill(Colors.TOP);
 		g.fillRect(x, y + width, width, 20);
+
+		/* Bow */
+		drawBow(g);
 	}
 
 	@Override
 	public void renderDebug(GraphicsContext g, double elapsed) {
 		super.renderDebug(g, elapsed);
 		Point2D center = getCenterPoint();
-		Point2D mouse = mouseInput.mouseLocation();
+		Point2D mouse = mouseInput.location();
+		double bowX = center.getX() - BOW_SIZE / 2;
+		double bowY = center.getY() - BOW_SIZE / 2;
+		g.setStroke(Color.WHITE);
+		g.strokeArc(bowX, bowY, BOW_SIZE, BOW_SIZE, 0, 360, ArcType.OPEN);
 		g.strokeLine(center.getX(), center.getY(), mouse.getX(), mouse.getY());
+		g.fillText(String.format("%fdeg", this.angleTo(mouse)), mouse.getX() + 10, mouse.getY());
 	}
 }
