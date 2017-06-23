ef5d6fff88b1cdff6c61ff6e6ff29f731edfecd3
HatScripts <contact@hatscripts.com>
2017-06-23 18:36:48 +1000

    Add window icon (placeholder)

--- src/main/java/com/hatscripts/archergame/window/Main.java
+++ src/main/java/com/hatscripts/archergame/window/Main.java
@@ -6,15 +6,19 @@ import javafx.application.Application;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main extends Application {
 	private Game game;
 
 	@Override
-	public void start(Stage stage) throws Exception {
+	public void start(Stage stage) {
 		Group root = new Group();
 		Scene scene = new Scene(root);
 
@@ -32,13 +36,18 @@ public class Main extends Application {
 		root.getChildren().add(game);
 
 		stage.setTitle("Archer Game");
+		stage.getIcons().addAll(
+				Stream.of(16, 24, 32, 48, 64, 128, 256)
+						.map(size -> new Image("logo-" + size + ".png"))
+						.collect(Collectors.toSet())
+		);
 		stage.setScene(scene);
 		stage.centerOnScreen();
 		stage.show();
 	}
 
 	@Override
-	public void stop() throws Exception {
+	public void stop() {
 		game.stopAnimationTimer();
 	}
 }
