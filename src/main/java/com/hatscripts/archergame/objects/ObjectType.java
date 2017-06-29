3f0ca67cbaa47da9c462fffd123073bb0d940d7a
HatScripts <contact@hatscripts.com>
2017-06-29 13:24:43 +1000

    Add createObject method which uses reflection

--- src/main/java/com/hatscripts/archergame/objects/ObjectType.java
+++ src/main/java/com/hatscripts/archergame/objects/ObjectType.java
@@ -1,41 +1,55 @@
 package com.hatscripts.archergame.objects;
 
+import com.hatscripts.archergame.input.KeyInput;
+import com.hatscripts.archergame.input.MouseInput;
+import com.hatscripts.archergame.objects.interfaces.GameObject;
+import com.hatscripts.archergame.objects.list.Block;
+import com.hatscripts.archergame.objects.list.Debug;
+import com.hatscripts.archergame.objects.list.Player;
 import javafx.geometry.Dimension2D;
 
-public enum ObjectType {
-	PLAYER("You", true, new Dimension2D(32, 52), 200),
-	BLOCK("Block", true, new Dimension2D(32, 32), 0);
-	private final String name;
-	private final boolean solid;
-	private final double width;
-	private final double height;
-	private final double maxSpeed;
+import java.lang.reflect.Constructor;
 
-	ObjectType(String name, boolean solid, Dimension2D dimension, double maxSpeed) {
+public enum ObjectType {
+	DEBUG(Debug.class, "Debug", new Dimension2D(0, 0)),
+	PLAYER(Player.class, "You", new Dimension2D(32, 52)),
+	BLOCK(Block.class, "Block", new Dimension2D(32, 32));
+	private final Class<? extends GameObject> clazz;
+	private final String name;
+	private final Dimension2D size;
+
+	ObjectType(Class<? extends GameObject> clazz, String name, Dimension2D size) {
+		this.clazz = clazz;
 		this.name = name;
-		this.solid = solid;
-		this.width = dimension.getWidth();
-		this.height = dimension.getHeight();
-		this.maxSpeed = maxSpeed;
+		this.size = size;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public boolean isSolid() {
-		return solid;
+	public Dimension2D getSize() {
+		return size;
 	}
 
-	public double getWidth() {
-		return width;
-	}
-
-	public double getHeight() {
-		return height;
-	}
-
-	public double getMaxSpeed() {
-		return maxSpeed;
+	public GameObject createObject(double x, double y, KeyInput keyInput, MouseInput mouseInput) {
+		@SuppressWarnings("unchecked") Constructor<? extends GameObject> constructor =
+				(Constructor<? extends GameObject>) clazz.getDeclaredConstructors()[0];
+		if (!constructor.isAccessible()) {
+			constructor.setAccessible(true);
+		}
+		try {
+			switch (constructor.getParameterCount()) {
+				case 4:
+					return constructor.newInstance(x, y, size.getWidth(), size.getHeight());
+				case 6:
+					return constructor.newInstance(x, y, size.getWidth(), size.getHeight(),
+							keyInput, mouseInput);
+				default:
+					throw new AssertionError();
+			}
+		} catch (ReflectiveOperationException e) {
+			throw new AssertionError(e);
+		}
 	}
 }
