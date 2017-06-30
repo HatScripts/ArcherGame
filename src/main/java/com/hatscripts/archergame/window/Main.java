4efa8f3ab929a20c8cc49363a0eecc2501fc1605
HatScripts <contact@hatscripts.com>
2017-06-30 16:18:19 +1000

    Add `main` method so that exe can launch

--- src/main/java/com/hatscripts/archergame/window/Main.java
+++ src/main/java/com/hatscripts/archergame/window/Main.java
@@ -16,6 +16,10 @@ import java.util.stream.Stream;
 public class Main extends Application {
 	private Game game;
 
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
 	@Override
 	public void start(Stage stage) {
 		Group root = new Group();
